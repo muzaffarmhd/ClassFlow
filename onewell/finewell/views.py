4bc9cdd06d4f008ef489845df0773117205e7912
muzaffarmhd <mdmuzaffarprof@gmail.com>
2023-10-22 11:39:17 +0530

    modified auth

--- onewell/finewell/views.py
+++ onewell/finewell/views.py
@@ -10,11 +10,13 @@ from django import forms
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ("username", "password1", "password2")
+        fields = ("username", "password1", "password2","first_name", "last_name")
     
     username = forms.CharField(label="Username")
     password1 = forms.CharField(widget=forms.PasswordInput, label="Password",)
     password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    first_name = forms.CharField(label="First Name")
+    last_name = forms.CharField(label="Last Name")
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
